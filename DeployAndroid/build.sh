7b74ff9abeec8dc8eab71e937aed9f80022b1a06
Artanidos <artanidos@gmail.com>
2020-04-30 17:43:48 +0100

    upd

--- DeployAndroid/build.sh
+++ DeployAndroid/build.sh
@@ -1,4 +1,4 @@
-export ANDROID_NDK_ROOT=/home/art/Android/Sdk/ndk-bundle
+export ANDROID_NDK_ROOT=/media/art/data/Sdk/ndk-bundle
 export ANDROID_NDK_PLATFORM=android-28
-export ANDROID_SDK_ROOT=/home/art/Android/Sdk
-python3.7 build.py --target android-32 --installed-qt-dir /home/art/Qt/5.12.3 --no-sysroot --verbose --source-dir ./external-sources
+export ANDROID_SDK_ROOT=/media/art/data/Sdk
+python3.7 build.py --target android-32 --installed-qt-dir /home/art/Qt/5.12.3 --verbose --source-dir ./external-sources
